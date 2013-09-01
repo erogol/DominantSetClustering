6aed4b49693f00366a0988978e0361b42e00dc5a
Eren Golge <erogol@hotmail.com>
2013-09-01 23:15:22 +0200

    Update create_sim_matrix.m

--- create_sim_matrix.m
+++ create_sim_matrix.m
@@ -1,4 +1,4 @@
 function[S] = create_sim_matrix(DATA)
-S = pdist2(DATA(:,1:2),DATA(:,1:2),'seuclidean');
+S = pdist2(DATA(:,1:end),DATA(:,1:end),'seuclidean');
 sigma2 = median(S(:));
 S = exp(-S/sigma2);
