721d80b8722060a329f46707035245193ca1f5ba
Eren Golge <erogol@hotmail.com>
2013-12-24 16:56:36 +0200

    Update perform_clustering.m

--- perform_clustering.m
+++ perform_clustering.m
@@ -6,5 +6,7 @@ read_all_data();
 DATA = DATA1;
 S = create_sim_matrix(DATA);
 
-[clusters charVectors prototypeIndices payoffs nCluster] = clusterDS(S, 'MaxClust', 3);
+% Keep in mind that 'MaxClust' is the number of dominant set will be extracted
+% Hence if 'MaxClust' is 1 you will have 2 clusters with the residual data
+[clusters charVectors prototypeIndices payoffs nCluster] = clusterDS(S, 'MaxClust', 1);
 plot_data(DATA(:,1:2), clusters);
