e4865c9d12a053cd12358face1cbfac9772cfe2d
Eren Golge <erogol@hotmail.com>
2014-01-30 03:08:45 +0200

    Update perform_clustering.m

--- perform_clustering.m
+++ perform_clustering.m
@@ -1,12 +1,8 @@
 % clusterDS example run
-
 config();
 read_all_data();
-
-DATA = DATA1;
-S = create_sim_matrix(DATA);
-
-% Keep in mind that 'MaxClust' is the number of dominant set will be extracted
-% Hence if 'MaxClust' is 1 you will have 2 clusters with the residual data
-[clusters charVectors prototypeIndices payoffs nCluster] = clusterDS(S, 'MaxClust', 1);
+DATA = DATA4;
+[S,dist_mat] = create_sim_matrix(DATA);
+[clusters charVectors prototypeIndices payoffs nCluster] = clusterDS(S, 'MaxClust', 3);
+fprintf('time spend %d\n', end_time)
 plot_data(DATA(:,1:2), clusters);
